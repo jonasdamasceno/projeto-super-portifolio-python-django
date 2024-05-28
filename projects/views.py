909ec96e8d7654c3918d323abb063d679673f954
jonasmichael <jonasmichael2021@outlook.com>
2024-05-28 10:45:25 -0300

    requisito 4

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from projects.models import Profile, Project
 from projects.serializers import ProfileSerializer, ProjectSerializer
 from rest_framework import viewsets, permissions
@@ -7,6 +8,18 @@ class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
+
+    def retrieve(self, request, *args, **kwargs):
+        if request.method == "GET":
+            profile = Profile.objects.get(id=kwargs["pk"])
+            return render(request, "profile_detail.html", {"profile": profile})
+
+        return super().retrieve(request, *args, **kwargs)
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
